e831f4ef1959891bb14a010b5f1907dbdcdad153
CHOICORE <choijaehyeong93@gmail.com>
2023-07-17 12:50:00 +0900

    refactor: change validation

--- authenticator/domain/src/main/kotlin/me/choicore/api/authenticator/domain/user/model/DateOfBirth.kt
+++ authenticator/domain/src/main/kotlin/me/choicore/api/authenticator/domain/user/model/DateOfBirth.kt
@@ -1,5 +1,6 @@
 package me.choicore.api.authenticator.domain.user.model
 
+import java.time.DateTimeException
 import java.time.LocalDate
 
 data class DateOfBirth(
@@ -7,10 +8,22 @@ data class DateOfBirth(
     val month: Int,
     val dayOfMonth: Int,
 ) {
+    init {
+        validateDateOfBirth(year, month, dayOfMonth)
+    }
+
     constructor(dateOfBirth: LocalDate) : this(
         year = dateOfBirth.year,
         month = dateOfBirth.monthValue,
         dayOfMonth = dateOfBirth.dayOfMonth
     )
+
+    private fun validateDateOfBirth(year: Int, month: Int, dayOfMonth: Int) {
+        try {
+            LocalDate.of(year, month, dayOfMonth)
+        } catch (e: DateTimeException) {
+            throw DateTimeException("Invalid date of birth: $year-$month-$dayOfMonth")
+        }
+    }
 
 }
