3cc94d49103dc0dfe3ef6dc570faccdee8c44109
CHOICORE <choijaehyeong93@gmail.com>
2023-08-05 20:15:17 +0900

    refactor: add module settings

--- authenticator/infrastructure/src/main/kotlin/me/choicore/api/authenticator/infrastructure/persistence/jpa/repository/UserRepository.kt
+++ authenticator/infrastructure/src/main/kotlin/me/choicore/api/authenticator/infrastructure/persistence/jpa/repository/UserRepository.kt
@@ -1,10 +1,11 @@
 package me.choicore.api.authenticator.infrastructure.persistence.jpa.repository
 
+import me.choicore.api.authenticator.domain.user.model.UserProfile
 import me.choicore.api.authenticator.domain.user.port.`in`.command.UserCommand
 import me.choicore.api.authenticator.domain.user.port.out.RegisterUserDrivenPort
 
 class UserRepository : RegisterUserDrivenPort {
-    override fun register(registerUser: UserCommand.RegisterUser): Long {
-        return 0L
+    override fun register(registerUser: UserCommand.RegisterUser): UserProfile {
+        TODO()
     }
 }
